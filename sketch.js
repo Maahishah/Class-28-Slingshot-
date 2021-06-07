0d0a5b4bb4f9907f9958e4696f8be462f54d23b1
maahishah <smaahi1777@gmail.com>
2021-06-07 16:35:46 -0400

    angry birds game

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
 
 var engine, world;
 var box1, pig1;
@@ -33,9 +36,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(230,180,80,PI/2);
     bird = new Bird(100,100);
 
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -60,5 +64,7 @@ function draw(){
     log5.display();
 
     bird.display();
+    log6.display();
+    chain.display();
     platform.display();
 }
